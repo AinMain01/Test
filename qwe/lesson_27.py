afdec1ee4dd93fff3cc07487d2698846cc5a2f28
AinMain01 <ainmain000@gmail.com>
2025-03-12 23:21:34 +0300

    Update lesson_27.py

--- qwe/lesson_27.py
+++ qwe/lesson_27.py
@@ -1,15 +1,19 @@
-n = list(set(map(int, input("Введите числа на карточках: ").split())))
-sacha, peta = 0, 0
-def summa(n):
-    maxi = max([n[0], n[-1]])
-    n.remove(maxi)
-    return maxi
-for i in range(len(n) // 2 +1):
-    if len(n) >= 2:
-        sacha += summa(n)
-        peta += summa(n)
-    else: sacha += n[0]
-print(sacha, peta)
-if sacha > peta: print("Саша")
-elif sacha < peta: print("Петя")
+n = int(input("Введите кол-во карточек: "))
+cards = list(map(int, input("Введите числа: ").split()))
+sasha, peta = 0, 0
+left, right = 0, n - 1
+
+for xod in range(n):
+    if cards[left] > cards[right]:
+        if xod % 2 == 0: sasha += cards[left]
+        else: peta += cards[left]
+        left += 1
+    else:
+        if xod % 2 == 0: sasha += cards[right]
+        else: peta += cards[right]
+        right -= 1
+
+print(sasha, peta)
+if sasha > peta: print("Саша")
+elif peta > sasha: print("Петя")
 else: print("Ничья!")
