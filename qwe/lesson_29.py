ecbacb457fb361894da7aa62e8ca29a95fb28e67
AinMain01 <ainmain000@gmail.com>
2025-03-18 17:45:56 +0300

    OOП

---
+++ qwe/lesson_29.py
@@ -0,0 +1,11 @@
+class Car:
+    mark = "Toyota"
+    model = "Carolla"
+    color = "blue"
+    speed = 0
+
+my_car_1 = Car()
+print(f"Марка автомобиля: {my_car_1.mark}")
+print(f"Модель автомобиля: {my_car_1.model}")
+print(f"Цвет автомобиля: {my_car_1.color}")
+print(f"Скорость автомобиля: {my_car_1.speed}")
