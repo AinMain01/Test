20d50a3fb01093c64a75da2a6456f332e7e9af51
AinMain01 <ainmain000@gmail.com>
2025-03-27 18:40:09 +0300

    lesson_30

--- qwe/kv_or_kb.py
+++ qwe/kv_or_kb.py
@@ -1,4 +1,4 @@
-n = [x for _ in range(int(input("Введите t (1<=t<=20): "))) if ((x := int(input("Введите число n (1<=n<=109): "))))]
+n = [x for i in range(int(input("Введите t (1<=t<=20): "))) if ((x := int(input("Введите число n (1<=n<=109): "))))]
 for a in n: print(len(sorted(list(set([x for i in range(1, int(a**0.5) +2) for x in (i**2, i**3) if x <= a])))))
 # a = int(input())
 # print(sorted(list(set([x for i in range(1, int(a**0.5) +2) for x in (i**2, i**3) if x <= a]))))
