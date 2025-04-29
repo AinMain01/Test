04bf620fea4a326b7f4280e7518d8657b6c7ddbb
AinMain01 <ainmain000@gmail.com>
2025-04-29 18:37:31 +0300

    Archer

--- qwe/hero_archer.py
+++ qwe/hero_archer.py
@@ -1,82 +1,38 @@
-class Hero:
-    def __init__(self, name="", healf=100, damage=1, defense=0):
-        self.name, self.healf, self.damage, self.defense = name, healf, damage, defense
+from random import randint
+class Human:
+    def __init__(self, name="", health=100):
+        self._name, self._health = name, health
 
-    def get_status(self):
-        print(f"Имя: {self.name} \nЗдоровье: {self.healf} \nУрон: {self.damage} \nЗащита: {self.defense}")
-        print("______________________________________")
-
-    def increase_defense(self):
-        if self.defense *1.5 < 100: self.defense *= 1.5
-        else: self.defense = 100
-        print(f"Текущий уровень защиты: {self.defense}")
-        print("______________________________________")
-
-    def make_damage(self, enemy):
-        print(f"Атака по персонажу {enemy.name}!")
-        print("______________________________________")
+    def attack(self, enemy):
+        print(f"{self._name} наносит персонажу {enemy} {enemy.get_damage()} урона!")
         enemy.get_damage(self.damage)
 
-    def get_damage(self, damage):
-        final_damage = damage - (damage * self.defense // 100)
-        print(f"По герою {self.name} нанесён урон {final_damage}")
-        self.healf -= final_damage
-        print("______________________________________")
+    def get_damage(self, damage=randint(1, 10)):
+        self._health -= damage
+        print(f"{self._name} получает {damage} урона! Текущее здоровье - {self._health}.")
 
-# hero_1 = Hero(name="Артур", damage=20, defense=5)
-# hero_2 = Hero("Робин", 80, 30, 4)
-# hero_1.get_status()
-# hero_1.increase_defense()
+    def healing(self):
+        healing = randint(5, 15)
+        self._health += healing
+        print(f"{self._name} восстанавливает {healing} здоровья. Текущее здоровье - {self._health}")
 
-# hero_2.get_status()
-# hero_2.make_damage(hero_1)
-# hero_1.get_status()
-
-from random import randint as rd
-
-class Archer:
-    def __init__(self, name="", healf=100, damage=1, metcost=0.1, lovk=1):
-        self.name, self.healf, self.damage, self.metcost, self.lovk = name, healf, damage, metcost, lovk
-
-    def get_status(self):
-        print(f"Имя: {self.name} \nЗдоровье: {self.healf} \nУрон: {self.damage} \nМеткость: {self.metcost} \nЛовкость: {self.lovk}")
-        print("______________________________________")
-
-    def level_up(self):
-        self.healf += 10
-        self.damage +=6
-        if self.metcost +0.05 < 2:
-            self.metcost += 0.05
-        else:
-            print(f"Достигнут лимит Меткости: {self.metcost}!")
-            print("______________________________________")
-
-        if self.lovk +2 < 25:
-            self.lovk += 2
-        else:
-            print(f"Достигнут лимит Ловкости: {self.lovk}!")
-            print("______________________________________")
-        print("Левел повышен!")
-        print("______________________________________")
-
-    def make_damage(self, enemy):
-        print(f"Атака по персонажу {enemy.name}!")
-        print("______________________________________")
-        enemy.get_damage(self.damage *self.metcost)
+class Warrior(Human):
+    def __init__(self, name, health, defense=0):
+        super().__init__(name, health)
+        self._defense = defense
     
-    def get_damage(self, damage):
-        chance_damage = rd(1, 100)
-        if self.lovk >= chance_damage: print(f"Лучник {self.name} увернулся от удара!")
-        else:
-            print(f"По герою {self.name} нанесён урон {damage}!")
-            self.healf -= damage
-        print("______________________________________")
+    def get_damage(self):
+        super().get_damage()
+        damage = randint(10, 20)
+        if damage - self._defense < 0: damage = 0
+        else: damage -= self._defense
 
-archer_1 = Archer(name="Артур", damage=5, metcost=0.06)
-archer_2 = Archer("Робин", 80, 30, 2, 25)
-archer_2.get_status()
-# archer_1.level_up()
-# archer_1.get_status()
+class Archer(Human):
+    def __init__(self, name, health, accuracy=0.0, agility=0):
+        super().__init__(name, health)
+        self._accuracy, self._agility = accuracy, agility
 
-archer_1.make_damage(archer_2)
-archer_2.get_status()
+    def get_damage(self):
+        super().get_damage()
+        damage = randint(15, 25) * self._accuracy
+        
--- qwe/lesson_37.py
+++ qwe/lesson_37.py
@@ -1,4 +1,5 @@
-class Rectanagle:
+from figure import Figure
+class Rectanagle(Figure):
     def __init__(self, lenght, width):
         self._lenght, self._widht = lenght, width
     
@@ -8,7 +9,7 @@ class Rectanagle:
     def get_perimeter(self):
         return (self._lenght + self._widht) * 2
 
-class Triangle:
+class Triangle(Figure):
     def __init__(self, site_1, site_2, site_3):
         self._site_1, self._site_2, self._site_3 = site_1, site_2, site_3
 
@@ -19,14 +20,24 @@ class Triangle:
     def get_perimeter(self):
         return self._site_1 + self._site_2 + self._site_3
     
-rect_1 = Rectanagle(4, 5)
+class Circle(Figure):
+    def __init__(self, radius):
+        super().__init__()
+        self.radius = radius
+    
+# rect_1 = Rectanagle(4, 5)
 # rect_2 = Rectanagle(8, 10)
-trian_1 = Triangle(3, 4, 5)
+# trian_1 = Triangle(3, 4, 5)
 # trian_2 = Triangle(3, 5)
 
 # print(f"{rect_1.get_area()} \n{rect_2.get_area()}")
 # print(f"{trian_1.get_area()} \n{trian_2.get_area()}")
 
-figures = [rect_1, trian_1]
-for fig in figures:
-    print(f"Площадь фигуры: {fig.get_area()} \nПериметр фигуры: {fig.get_perimeter()}")
+# figures = [rect_1, trian_1]
+# for fig in figures:
+#     print(f"Площадь фигуры: {fig.get_area()} \nПериметр фигуры: {fig.get_perimeter()}")
+    
+circle_1 = Circle(8)
+circle_1_area = circle_1.get_area()
+circle_1_perimeter = circle_1.get_perimeter()
+# print(f"{circle_1_area}")
