b023ad187782d80af84fc371a5e66b85210b78af
AinMain01 <ainmain000@gmail.com>
2025-05-19 22:46:32 +0300

    Ураааааа

--- guess_number.py
+++ guess_number.py
@@ -3,26 +3,31 @@ from random import randint
 
 class Game:
     def __init__(self):
+        self.reset_game()
+
+    def reset_game(self):
+        self.pc_choice = randint(1, 100)
         self.attempt = 0
+        print(self.pc_choice)
 
     def move_result(self, user_choice):
-        pc_choice = randint(1, 100)
-
         try:
-            if user_choice > 100 or user_choice < 1:
-                return f"Введите число от 1 до 100 вклюительно!"
-            elif user_choice == pc_choice:
+            val = user_choice.get()
+
+            if int(val) > 100 or int(val) < 1:
+                return "Введите число от 1 до 100 вклюительно!"
+            elif int(val) == self.pc_choice:
                 self.attempt += 1
-                return f"Ваше число - {user_choice}. Вы угадали!"
-            elif user_choice < pc_choice:
+                n = self.attempt
+                self.reset_game()
+                return f"Ваше число - {val}. Вы угадали за {n} попыток!"
+            if int(val) < self.pc_choice:
                 self.attempt += 1
-                return f"Ваше число - {user_choice}. Моё число больше!"
-            elif user_choice > pc_choice:
+                return f"Ваше число - {val}. Моё число больше!"
+            elif int(val) > self.pc_choice:
                 self.attempt += 1
-                return f"Ваше число - {user_choice}. Моё число меньше!"
-        except ValueError:
-            return f"Введите корректное число!"
-        else: return f"Неизвестная ошибка!"
+                return f"Ваше число - {val}. Моё число меньше!"
+        except ValueError: return f"Введите корректное число!"
         
     def show_info(self):
         return f"Попыток - {self.attempt}"
@@ -44,16 +49,16 @@ class GUI:
         self.lb_1.grid(row=0, column=0, columnspan=3)
 
         self.lb_2 = Label(window, text=self.game.show_info() ,font=("Arial", 13))
-        self.lb_2.grid(row=1, column=0)
+        self.lb_2.grid(row=0, column=0, columnspan=3, rowspan=2)
 
         self.lb_3 = Label(window, text="Сначала введите число", font=("Arial", 13))
-        self.lb_3.grid(row=4, column=0)
+        self.lb_3.grid(row=2, column=0, columnspan=3)
 
         self.button = Button(window, text="Проверить", font=("Arial", 15), command=lambda: self.button_click(self.user))
-        self.button.grid(row=3, column=0)
+        self.button.grid(row=1, column=0, columnspan=3, rowspan=2)
 
         self.user = Entry(width=10)
-        self.user.grid(row=2, column=0)
+        self.user.grid(row=1, column=0, columnspan=3)
 
     def button_click(self, user_choice):
         self.lb_3["text"] = self.game.move_result(user_choice)
